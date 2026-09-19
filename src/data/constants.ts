e00e95f308ccb61efad2659d03254fa74ef2734a
cabreltech <cabcabrel@gmail.com>
2026-09-20 00:15:16 +0100

    Fix: include src/data/constants.ts and restrict ignored data dir to root

---
+++ src/data/constants.ts
@@ -0,0 +1,22 @@
+export const EVENT_TYPES = [
+  'Mariage',
+  'Anniversaire',
+  'Dot / Cérémonie',
+  'Événement d\'entreprise',
+  'Soirée privée / Fête',
+  'Autre'
+];
+
+export const POPULAR_BEER_BRANDS = [
+  '33 Export',
+  'Castel Beer',
+  'Beaufort Light',
+  'Beaufort Lager',
+  'Mutzig',
+  'Guinness',
+  'Kadji Beer',
+  'Isenbeck',
+  'Booster Cider'
+];
+
+export const PRESET_KEGS = [1, 2, 3, 5, 8, 10];
